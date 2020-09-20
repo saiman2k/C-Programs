0d135022747b23e5c9eac938da5e8aa6259ad1ea
saiman2k <70460655+saiman2k@users.noreply.github.com>
2020-09-20 11:40:27 +0530

    Create Arithmetic-op.c

---
+++ Arithmetic-op.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a = 6013,b = 13, c;
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("Remainder = %d \n",c);
+    return 0;
+}
